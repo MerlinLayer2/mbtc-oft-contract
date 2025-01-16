8a3ad796a0ae29390adb95c98f1b56fc39b5efcc
j <j@x.x>
2025-01-16 13:29:25 +0800

    feat: token deploy name

--- deploy/MyOFT.ts
+++ deploy/MyOFT.ts
@@ -43,8 +43,8 @@ const deploy: DeployFunction = async (hre) => {
     const { address } = await deploy(contractName, {
         from: deployer,
         args: [
-            'MyOFT', // name
-            'MOFT', // symbol
+            'Merlin BTC', // name
+            'MBTC', // symbol
             endpointV2Deployment.address, // LayerZero's EndpointV2 address
             deployer, // owner
         ],
